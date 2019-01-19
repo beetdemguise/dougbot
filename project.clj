ba677fa9ee5c8629d4f45f2948d35024eb94aac9
Darin Douglass <douglassdarin@gmail.com>
2019-01-18 21:02:48 -0500

    Add terraria command

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.490"]
                  [discord.clj "1.1.1"]
                  [amazonica "0.3.139"]
                  [mount "0.1.15"]]
--- src/dougbot/commands/terraria.clj
+++ src/dougbot/commands/terraria.clj
@@ -1,44 +1,73 @@
 (ns dougbot.commands.terraria
   (:require [amazonica.aws.ec2 :as aws]
-            [discord.bot :as discord]))
+            [clojure.core.async :as async]
+            [discord.bot :as bot]
+            [discord.embeds :as embed]
+            [discord.http :as discord]))
+
+(defn get-instance-info []
+  (get-in (aws/describe-instances :filters [{:key "tag:name"
+                                             :value "Terraria"}])
+          [:reservations 0 :instances 0]))
 
 (def terraria-instance-id
-  (get-in (aws/describe-instances :filters [{:key "tag:name" :value "Terraria"}])
-          [:reservations 0 :instances 0 :instance-id]))
+  (:instance-id (get-instance-info)))
 
-(defn is-running? []
+(defn get-status []
   (as-> terraria-instance-id $
     (aws/describe-instance-status :instance-id $)
     (:instance-statuses $)
-    (drop-while #(and (not= (:instance-id %))
-                      ;; 0 = pending
-                      ;; 16 = running
-                      (not (contains? [0 16] (get-in % [:instance-state :code]))))
-                $)
-    (seq $)))
+    (drop-while #(not= (:instance-id %)) $)
+    (first $)
+    (get-in $ [:instance-state :code])))
 
-(discord/defextension terraria [client message]
+(defn is-pending? []
+  (= 0 (get-status)))
+
+(defn is-running? []
+  (= 16 (get-status)))
+
+(defn build-status-message []
+  (let [instance-emote (format ":%s:" (condp = (get-status)
+                                        0 "blue_circle"
+                                        16 "white_check_mark"
+                                        "red_circle"))
+        ip-address (:public-ip-address (get-instance-info))
+        text (str instance-emote (when ip-address
+                                   (format "IP Address `%s`" ip-address)))]
+    (-> (embed/create-embed :title "Terraria Status")
+        (embed/+field "EC2 Instance Status" text))))
+
+(defn send-status-message [auth channel]
+  (discord/send-message auth channel "" :embed (build-status-message)))
+
+(defn update-status-message [auth channel message]
+  (discord/edit-message auth channel message "" :embed (build-status-message)))
+
+(bot/defextension terraria [{:keys [auth]} {:keys [channel]}]
   "Commands to interact with the Terraria EC2 instance"
   (:status
    "Shows the current status of the instance"
-   (discord/say (if (is-running?)
-                  "Instance is currently up-and-running."
-                  "The instance is not started.")))
+   (bot/say (build-status-message)))
 
   (:start
    "Starts the instance"
    (if (is-running?)
-     (discord/say "Instance is already running.")
-     (do
+     (bot/say (build-status-message))
+     (let [_ (bot/say "Starting up Terraria instance...")
+           bot-message (send-status-message auth channel)]
        (aws/start-instances :instance-ids [terraria-instance-id])
-       ;; TODO: start actual tshock server
-       (discord/say "Instance is running"))))
+       (while (not (is-running?))
+         (async/<!! (async/timeout 5000)))
+       (update-status-message auth channel bot-message))))
 
   (:stop
    "Stops the instance"
    (if-not (is-running?)
-     (discord/say "Instance isn't running.")
-     (do
-       ;; TODO: stop tshock server before stopping instance
+     (bot/say (build-status-message))
+     (let [_ (bot/say "Stopping Terraria instance...")
+           bot-message (send-status-message auth channel)]
        (aws/stop-instances :instance-ids [terraria-instance-id])
-       (discord/say "Instance is stopping.")))))
+       (while (some? (get-status))
+         (async/<!! (async/timeout 5000)))
+       (update-status-message auth channel bot-message)))))
